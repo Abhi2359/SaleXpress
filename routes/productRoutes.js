610967b605d28d9d691925ddb215c1eb72594fc9
Abhi2359 <abhishekchoudhary1018@gmail.com>
2023-05-17 17:51:22 +0530

    category from

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -1,10 +1,45 @@
-import express from "express"
+import express from "express";
 import { requireSignIn, isAdmin } from "../middlewares/authMiddleware.js";
-import { createProductController } from "../controllers/productController.js";
+import {
+  createProductController,
+  deleteProductController,
+  getProductController,
+  getSingleProductController,
+  productPhotoController,
+  updateProductController,
+} from "../controllers/productController.js";
+import formidable from "express-formidable";
 const router = express.Router();
 
 //Routes
-router.post("/create-product", requireSignIn,isAdmin,createProductController)
+// Create Product
+router.post(
+  "/create-product",
+  requireSignIn,
+  isAdmin,
+  formidable(),
+  createProductController
+);
+// Update Product
 
+router.put(
+  "/update-product/:pid",
+  requireSignIn,
+  isAdmin,
+  formidable(),
+  updateProductController
+);
+
+//get All Products
+router.get("/get-products", getProductController);
+
+// get Single Product
+router.get("/get-product/:slug", getSingleProductController);
+
+// get Product Photo
+router.get("/product-photo/:pid", productPhotoController);
+
+// delete Product
+router.delete("/delete-product/:pid", deleteProductController);
 
 export default router;
